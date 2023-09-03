4c6c3592db816d092c12e94b4b7dafc69c543c4e
Flutter Devolper <101264641+AkshayMohanMp@users.noreply.github.com>
2023-09-03 17:53:39 +0530

    server.js
    updated

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { Student, Class } = require('./models/productModel');
+const Student = require('./models/productModel'); 
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.set("strictQuery",false)
-mongoose.
-connect('mongodb+srv://admin:Akshay123@cluster0.iqf9kfk.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(()=>{
-    console.log('Connected to MongoDB')
-  
-}).catch((error)=>{
-    console.log(error)
-})
+mongoose.set('strictQuery', false);
+
+mongoose
+    .connect('mongodb+srv://admin:AKshay123@cluster0.iqf9kfk.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 app.use(bodyParser.json());
 
 app.listen(3006, () => {
     console.log(`Server is running on port ${3006}`);
-});
-
-
-app.post('/api/classes', async (req, res) => {
-    try {
-        const classData = req.body;
-        const newClass = await Class.create(classData);
-        res.json(newClass);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to create class' });
-    }
-});
-
-
-app.get('/api/classes', async (req, res) => {
-    try {
-        const classes = await Class.find({});
-        res.json(classes);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to retrieve classes' });
-    }
 });
 
 
@@ -54,6 +34,7 @@ app.post('/api/students', async (req, res) => {
         res.status(500).json({ error: 'Failed to create student' });
     }
 });
+
 
 app.get('/api/students', async (req, res) => {
     try {
@@ -68,22 +49,11 @@ app.get('/api/students', async (req, res) => {
 app.put('/api/students/:id', async (req, res) => {
     try {
         const studentId = req.params.id;
-        const { classId } = req.body;
-        const updatedStudent = await Student.findByIdAndUpdate(studentId, { classId }, { new: true });
+        const updatedData = req.body;
+        const updatedStudent = await Student.findByIdAndUpdate(studentId, updatedData, { new: true });
         res.json(updatedStudent);
     } catch (error) {
         res.status(500).json({ error: 'Failed to update student' });
-    }
-});
-
-
-app.delete('/api/classes/:id', async (req, res) => {
-    try {
-        const classId = req.params.id;
-        await Class.findByIdAndRemove(classId);
-        res.json({ message: 'Class deleted' });
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to delete class' });
     }
 });
 
@@ -98,3 +68,24 @@ app.delete('/api/students/:id', async (req, res) => {
     }
 });
 
+
+app.get('/api/students/:standard', async (req, res) => {
+    try {
+        const standard = req.params.standard;
+        const students = await Student.find({ Standard: standard });
+        res.json(students);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve students' });
+    }
+});
+
+
+app.get('/api/students/:standard/:division', async (req, res) => {
+    try {
+        const {standard,division} = req.params;
+        const students = await Student.find({ Standard: standard, Division: division });
+        res.status(200).json(students);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve students' });
+    }
+})
